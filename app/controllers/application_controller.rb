cbba75dcf22b08f949617a537271467cfd3b1106
Dan Blondell <danblondell@gmail.com>
2017-12-11 05:04:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,13 +22,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    erb :error
-    # 
-    # if User.find(session[:user_id])
-    #   erb :account
-    # else
-    #   erb :error
-    # end
+    if User.find(session[:user_id])
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
